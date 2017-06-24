056bd7ee1046fc760a27b2bf110132d37cd19680
HadManySons <hadmanysons@gmail.com>
2017-06-24 13:33:35 +0900

    More descriptive exception handling

--- AFILinkerBot.py
+++ AFILinkerBot.py
@@ -271,9 +271,9 @@ while True:
         exit(0)
     
     except Exception as err:
-        print("Exception: " + err)
+        print("Exception: " + str(err.with_traceback()))
         logging.error(time.strftime("%Y/%m/%d %H:%M:%S ") 
-                                + "Unhandled exception: " + err)
+                                + "Unhandled exception: " + + str(err.with_traceback()))
 
     finally:
         os.unlink(pidfile)
