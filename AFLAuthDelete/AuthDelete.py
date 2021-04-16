edafc20f63a1cf7326ee5f895c5c583451aa7fd5
HadManySons <hadmanysons@gmail.com>
2021-04-15 21:15:52 -0500

    Update AuthDelete.py

--- AFLAuthDelete/AuthDelete.py
+++ AFLAuthDelete/AuthDelete.py
@@ -8,18 +8,6 @@ import sys
 # Doc page
 logging.basicConfig(filename='AuthDelete.log', level=logging.INFO)
 
-# Get the PID of this process
-pid = str(os.getpid())
-pidfile = "AuthDelete.pid"
-
-# Exit if a version of the script is already running
-if os.path.isfile(pidfile):
-    print(pidfile + " already running, exiting")
-    sys.exit()
-
-# Create the lock file for the script
-open(pidfile, 'w').write(pid)
-
 logging.info(time.strftime("%Y/%m/%d %H:%M:%S ") + "Starting script")
 
 credsPassword = os.environ.get('AFL_PASSWORD')
@@ -115,6 +103,3 @@ while True:
         print("Exception: " + str(err.with_traceback()))
         logging.error(time.strftime("%Y/%m/%d %H:%M:%S ")
                       + "Unhandled exception: " + + str(err.with_traceback()))
-
-    finally:
-        os.unlink(pidfile)
